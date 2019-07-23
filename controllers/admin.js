517c1742be2255b673e1cfab875b62419d3b8735
vano_varderesyan <vano.varderesyan@mail.ru>
2019-07-24 00:35:02 +0400

    delete comment

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,24 +4,18 @@ const bookshelf = require('bookshelf')(knex);
 const Chats = bookshelf.Model.extend({
     tableName: 'chats',
     messages: function () {
-        return this.hasMany(Messages) //make the relationship with hardwiredinput
+        return this.hasMany(Messages)
     }
 });
 
-
-
-//create the hardwired input model
 const Files = bookshelf.Model.extend({
     tableName: 'files'
 });
 
-
-
-//retrieve the first equipment and all its related hardwiredinputs
 const Messages = bookshelf.Model.extend({
     tableName: 'messages',
     files: function () {
-        return this.hasMany(Files) //make the relationship with hardwiredinput
+        return this.hasMany(Files) 
     }
 });
 
@@ -32,15 +26,13 @@ module.exports = {
             .then(users => res.status(200).json(users))
             .catch(error => res.status(400).json(error))
     },
+    //find all chats
     findAllChats(req, res) {
         return Chats.where('id','>', 0).fetchAll({ withRelated: ['messages.files'] })
             .then((users) => {
-                // console.log(users.related('messages').toJSON());
-
                 res.status(200).json(users)
             })
             .catch((error) => {
-                console.log(error)
                 res.status(400).json(error)
             })
     },
